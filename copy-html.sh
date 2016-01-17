b4cbe3bf8809edeff8b12af95c58dda18c9a20ee
Kevin Dunn <kgdunn@gmail.com>
2016-01-17 16:35:52 +0100

    Cleaned up copying file

--- copy-html.sh
+++ copy-html.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python create_permalinks.py
+#python create_permalinks.py
 rsync -avvz --delete _build/html/*    		connectmv.com:webapps/newlearnche/pid/
 rsync -avvz --delete _build/latex/PID.pdf    	connectmv.com:webapps/newlearnche/pid/
 scp htaccess connectmv.com:webapps/yint_redirect/pid/.htaccess
