94f1ba8988ef80ef4a8c64ab1ce0884b21fb014a
Kevin Dunn <kgdunn@gmail.com>
2016-10-26 13:36:15 +0200

    Update Makefile and conf.py

--- conf.py
+++ conf.py
@@ -206,7 +206,7 @@ html_title = "Process Improvement using Data"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'textbook-logo-no-text-lowres.jpg'
+html_logo = 'preface/textbook-logo-no-text-lowres.jpg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
