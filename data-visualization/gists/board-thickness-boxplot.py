d0eb25e7a098eb6f1179cd98a647de395d62b66a
Kevin Dunn <kgdunn@gmail.com>
2020-04-30 19:55:22 +0200

    Adding some source code in gists, outside the RST files, with a view towards having both R and Python code snippets available in the book.

---
+++ data-visualization/gists/board-thickness-boxplot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+all_boards = pd.read_csv("https://openmv.net/file/six-point-board-thickness.csv")
+boards = all_boards.iloc[0:100, 1:7]
+
+# Look at the start and end of the data
+# Examine and summarize your data before
+# doing anything else
+boards.head()
+boards.tail()
+boards.describe()
+ax = boards.plot.box()
+plt.show()
---
+++ data-visualization/gists/quarterly-profit-barplots.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+labels = ["2008 Q1", "Q2", "Q3", "Q4", "2009 Q1", "Q2", "Q3", "Q4"]
+profit = (
+    pd.DataFrame(
+        data=[45, 32, 67, 23, 42, 56, 64, 92], index=labels, columns=["Quarterly profit ($ '000)"]
+    )
+    + 40
+)
+
+# # Draw a bar-plot
+ax = profit.plot.bar()
+ax.set_ylabel("Quarterly profit ($ '000)")
+plt.show()
+
+# Now rather use a line plot.
+ax = profit.plot.line(marker="o")
+ax.set_ylabel("Quarterly profit ($ '000)")
+plt.show()
