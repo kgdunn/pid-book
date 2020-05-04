39930e8e6d8b6939fc0be222999e6d4aa9d55e14
Kevin Dunn <kgdunn@gmail.com>
2020-05-04 22:40:59 +0200

    Have to explicitly create a second figure window.

--- data-visualization/gists/scatter-plot-example.py
+++ data-visualization/gists/scatter-plot-example.py
@@ -16,9 +16,9 @@ plt.show()
 #    density (measurement of osteoporosis)
 #    vs kg/m^3 (1500 kg/m3 is typical)
 N = 50
-
 white_hairs = np.random.normal(loc=500, scale=150, size=N)
 bone_mineral_density = -0.25 * white_hairs + 1550 + np.random.normal(loc=0, scale=25, size=N)
+plt.figure(1)
 plt.plot(white_hairs, bone_mineral_density, ".")
 plt.xlabel("Number of white hairs per square inch of scalp")
 plt.ylabel("Bone mineral density (kg/m$^3$) [measure of osteoporosis]")
