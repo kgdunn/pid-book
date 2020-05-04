ecdb416ebafb19377eed7f67e69e3a715ced6efa
Kevin Dunn <kgdunn@gmail.com>
2020-05-04 22:50:25 +0200

    Updated marker symbol

--- data-visualization/gists/scatter-plot-example.py
+++ data-visualization/gists/scatter-plot-example.py
@@ -19,7 +19,7 @@ N = 50
 white_hairs = np.random.normal(loc=500, scale=150, size=N)
 bone_mineral_density = -0.25 * white_hairs + 1550 + np.random.normal(loc=0, scale=25, size=N)
 fig2, ax2 = plt.subplots(nrows=1, ncols=1)
-ax2.plot(white_hairs, bone_mineral_density, ".", ms=10)
+ax2.plot(white_hairs, bone_mineral_density, "o", ms=10)
 ax2.set_xlabel("Number of white hairs per square inch of scalp")
 ax2.set_ylabel("Bone mineral density (kg/m$^3$) [measure of osteoporosis]")
 plt.show()
