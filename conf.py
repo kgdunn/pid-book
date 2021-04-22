3ce7787f10565a31cb8eef1f4eb5a55168278a02
Kevin Dunn <kgdunn@gmail.com>
2021-04-22 23:17:21 +0200

    Update settings to exclude notebooks directory.

--- conf.py
+++ conf.py
@@ -112,7 +112,7 @@ del cmd, out, subprocess
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", ".hg", "ext", "DELETE"]
+exclude_patterns = ["_build", ".hg", "ext", "DELETE", "applications-process-improvement/notebooks"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
