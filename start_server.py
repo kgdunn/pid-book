26a2ac2999175ead94f7df1f8ab19850c3b67404
Kevin Dunn <kgdunn@gmail.com>
2022-01-02 14:15:23 +0100

    Add script to run a local testing server.

---
+++ start_server.py
@@ -0,0 +1,31 @@
+import http.server
+import socketserver
+
+PORT = 8080
+
+
+class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    extensions_map = {
+        #'': 'application/octet-stream',
+        "": "text/html",
+        ".html": "text/html",
+        ".manifest": "text/cache-manifest",
+        ".html": "text/html",
+        ".png": "image/png",
+        ".jpg": "image/jpg",
+        ".svg": "image/svg+xml",
+        ".css": "text/css",
+        ".js": "application/x-javascript",
+        ".wasm": "application/wasm",
+        ".json": "application/json",
+        ".xml": "application/xml",
+    }
+
+
+httpd = socketserver.TCPServer(("localhost", PORT), HttpRequestHandler)
+
+try:
+    print(f"serving at http://localhost:{PORT}")
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
